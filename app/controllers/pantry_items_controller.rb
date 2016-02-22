6a511e31ec3b0aaf65edf2a1d7566ed31ba098a8
Paul <wheresmyjetpack03@gmail.com>
2016-02-21 23:39:41 -0600

    Created index for pantry_items

--- app/controllers/pantry_items_controller.rb
+++ app/controllers/pantry_items_controller.rb
@@ -1,9 +1,25 @@
 class PantryItemsController < ApplicationController
+  def index
+    @pantry_items = PantryItem.all
+  end
+
+  def show
+    @pantry_item = PantryItem.find(params[:id])
+  end
+
   def new
     @pantry_item = PantryItem.new
   end
 
   def create
-    render plain: params[:pantry_item].inspect
+    @pantry_item = PantryItem.new(pantry_item_params)
+
+    @pantry_item.save
+    redirect_to @pantry_item
+  end
+
+  private
+  def pantry_item_params
+    params.require(:pantry_item).permit(:name, :amount)
   end
 end
