4c5ea38028835909987393938cdeb63ccadc8489
Paul <wheresmyjetpack03@gmail.com>
2016-03-02 23:47:49 -0600

    Test for ingredient name required

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,6 @@
 class Ingredient < ActiveRecord::Base
   has_many :shopping_list_ingredients
   has_many :shoping_lists, :through => :shopping_list_ingredients
+
+  validates :name, presence: true
 end
--- test/models/ingredient_test.rb
+++ test/models/ingredient_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class IngredientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save ingredient without name" do
+    ingredient = Ingredient.new
+    assert_not ingredient.save
+  end
 end
