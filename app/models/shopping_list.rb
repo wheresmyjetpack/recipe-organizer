1a788abab18af6b597a382fd7eb696da03f2a7b0
Paul <wheresmyjetpack03@gmail.com>
2016-03-05 02:51:54 -0600

    Only add ingredients to shopping_list with non-existing names

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -1,7 +1,17 @@
 class ShoppingList < ActiveRecord::Base
   has_many :shopping_list_ingredients
   has_many :ingredients, :through => :shopping_list_ingredients
-
+    
   accepts_nested_attributes_for :shopping_list_ingredients, allow_destroy: :true
   accepts_nested_attributes_for :ingredients
+
+  before_save :find_or_create_ingredients
+
+  private
+  def find_or_create_ingredients
+    t = []
+    self.ingredients.each { |i| t << Ingredient.find_or_create_by(name: i.name) }
+    self.ingredients.replace(t)
+  end
+
 end
--- test/models/shopping_list_test.rb
+++ test/models/shopping_list_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ShoppingListTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "ingredients with name of existing ingredient should not save" do
+    # setup
+    ingredient1 = Ingredient.new(name: 'Milk')
+    ingredient1.save
+    
+    assert_no_difference 'Ingredient.count', 'Ingredient should not save' do
+      shopping_list = ShoppingList.new
+
+      shopping_list.ingredients.build(name: 'Milk')
+      shopping_list.save
+    end  
+  end
 end
