47843c5fe5266b9b6c4c3e75371012e85191a002
Paul <wheresmyjetpack03@gmail.com>
2016-04-22 21:53:45 -0500

    fixed uniqueness error in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-\.]+(?<!\.)\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: case_sensitive: false
+                    uniqueness:, case_sensitive: false
 end
