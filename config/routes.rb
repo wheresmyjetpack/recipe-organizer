6036d98473ae59d0974dcd2723e26b6d4833ec0d
Paul <wheresmyjetpack03@gmail.com>
2016-04-22 22:30:05 -0500

    changed elements, links in nav

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   resources :pantry_items
   resources :shopping_lists
+  resources :recipes
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
